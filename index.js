eb86454d78e413824d859104aaef7561aa363b0f
Gabu <gabrielleon9928@gmail.com>
2025-07-11 18:14:45 +0300

    modified prompt

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const geminiai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
 const aiPrompt = async () => {
   const { text: responseText } = await geminiai.models.generateContent({
     model: `gemini-2.5-flash`,
-    contents: `What is AI?`,
+    contents: `Who is Lebron James?`,
   });
   console.log(responseText);
 };
